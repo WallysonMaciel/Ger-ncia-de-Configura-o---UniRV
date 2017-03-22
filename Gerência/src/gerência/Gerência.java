c28fd5318d8ac797ad7e279aa0228853047e060e
Fiscal2 <Fiscal2@Fiscal2>
2017-03-22 15:45:07 -0300

    Adicionando JOptionPane para fazer somas das variaveis

--- Gerência/src/gerência/Gerência.java
+++ Gerência/src/gerência/Gerência.java
@@ -6,7 +6,13 @@ public class Gerência {
         int soma;
         int num1 = 0;
         int num2 = 0;
+        num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um numero:"));
 
+        num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite outro numero:"));
+
+        soma = num1 + num2;
+        JOptionPane.showMessageDialog(null, "A soma dos numeros e: " + soma);
+        System.exit(0);
         // TODO code application logic here
     }
 
