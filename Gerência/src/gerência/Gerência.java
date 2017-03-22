56495198ae20acb58a5437232433ade997805c7a
Fiscal2 <Fiscal2@Fiscal2>
2017-03-22 15:43:30 -0300

    Delaracao de variaveis

--- Gerência/src/gerência/Gerência.java
+++ Gerência/src/gerência/Gerência.java
@@ -1,12 +1,13 @@
 package gerência;
 
-
 public class Gerência {
 
- 
     public static void main(String[] args) {
-         
+        int soma;
+        int num1 = 0;
+        int num2 = 0;
+
         // TODO code application logic here
     }
-    
+
 }
